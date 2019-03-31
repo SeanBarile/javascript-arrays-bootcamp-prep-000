979dd92fd42a9c20be0379fff2aa3c79338d85f5
Sean Barile <aquariussb84@gmail.com>
2019-03-31 18:58:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,3 +30,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
+
+function descrutivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
